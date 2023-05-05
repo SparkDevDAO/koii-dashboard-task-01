583c9c33e32f99e69392b2a6e9c6dea6029a239a
Okami <luciano@sparksolutions.cloud>
2023-05-05 16:39:08 -0300

    initial task boilerplate

---
+++ coreLogic.js
@@ -0,0 +1,289 @@
+const { namespaceWrapper } = require('./namespaceWrapper');
+const { Web3Storage, File } = require('web3.storage');
+const axios = require('axios');
+
+class CoreLogic {
+  async task() {
+    // Write the logic to do the work required for submitting the values and optionally store the result in levelDB
+    // Below is just a sample of work that a task can do
+    try {
+      const storageClient = new Web3Storage({
+        token: process.env.WEB3_STORAGE_KEY,
+      });
+
+      const result = await axios.get(process.env.GET_K2_NODES_URL);
+      // console.log('Axios result data: ', result.data.data);
+
+      // TODO sign data uploaded to IPFS to prove node uploaded it
+      const resultSigned = await namespaceWrapper.payloadSigning(
+        JSON.stringify(result.data.data),
+      );
+      if (storageClient) {
+        const blob = new Blob([resultSigned], {
+          type: 'application/json',
+        });
+        const files = [new File([blob], 'data.json')];
+        // Storing on IPFS through web3 storage as example
+        const cid = await storageClient.put(files);
+        await namespaceWrapper.storeSet('cid', cid);
+        console.log('cid: ', cid);
+
+        // await namespace.checkSubmissionAndUpdateRound(cid);
+      } else {
+        console.error('No web3 storage API key provided');
+      }
+    } catch (err) {
+      console.log('ERROR IN EXECUTING TASK', err);
+    }
+  }
+  async fetchSubmission() {
+    // Write the logic to fetch the submission values here and return the cid string
+
+    // fetching round number to store work accordingly
+
+    // console.log('IN FETCH SUBMISSION');
+
+    // // The code below shows how you can fetch your stored value from level DB
+    try {
+      const cid = await namespaceWrapper.storeGet('cid');
+      return cid;
+    } catch (err) {
+      console.log('Error', err);
+      return err;
+    }
+  }
+
+  async generateDistributionList(round, _dummyTaskState) {
+    try {
+      console.log('GenerateDistributionList called');
+      console.log('I am selected node');
+
+      // Write the logic to generate the distribution list here by introducing the rules of your choice
+
+      /*  **** SAMPLE LOGIC FOR GENERATING DISTRIBUTION LIST ******/
+
+      let distributionList = {};
+      let distributionCandidates = [];
+      let taskAccountDataJSON = await namespaceWrapper.getTaskState();
+      if (taskAccountDataJSON == null) taskAccountDataJSON = _dummyTaskState;
+      const submissions = taskAccountDataJSON.submissions[round];
+      const submissions_audit_trigger =
+        taskAccountDataJSON.submissions_audit_trigger[round];
+      if (submissions == null) {
+        console.log('No submisssions found in N-2 round');
+        return distributionList;
+      } else {
+        const keys = Object.keys(submissions);
+        const values = Object.values(submissions);
+        const size = values.length;
+        console.log('Submissions from last round: ', keys, values, size);
+
+        // Logic for slashing the stake of the candidate who has been audited and found to be false
+        for (let i = 0; i < size; i++) {
+          const candidatePublicKey = keys[i];
+          if (
+            submissions_audit_trigger &&
+            submissions_audit_trigger[candidatePublicKey]
+          ) {
+            console.log(
+              'distributions_audit_trigger votes ',
+              submissions_audit_trigger[candidatePublicKey].votes,
+            );
+            const votes = submissions_audit_trigger[candidatePublicKey].votes;
+            if (votes.length === 0) {
+              // slash 70% of the stake as still the audit is triggered but no votes are casted
+              // Note that the votes are on the basis of the submission value
+              // to do so we need to fetch the stakes of the candidate from the task state
+              const stake_list = taskAccountDataJSON.stake_list;
+              const candidateStake = stake_list[candidatePublicKey];
+              const slashedStake = candidateStake * 0.7;
+              distributionList[candidatePublicKey] = -slashedStake;
+              console.log('Candidate Stake', candidateStake);
+            } else {
+              let numOfVotes = 0;
+              for (let index = 0; index < votes.length; index++) {
+                if (votes[index].is_valid) numOfVotes++;
+                else numOfVotes--;
+              }
+
+              if (numOfVotes < 0) {
+                // slash 70% of the stake as the number of false votes are more than the number of true votes
+                // Note that the votes are on the basis of the submission value
+                // to do so we need to fetch the stakes of the candidate from the task state
+                const stake_list = taskAccountDataJSON.stake_list;
+                const candidateStake = stake_list[candidatePublicKey];
+                const slashedStake = candidateStake * 0.7;
+                distributionList[candidatePublicKey] = -slashedStake;
+                console.log('Candidate Stake', candidateStake);
+              }
+
+              if (numOfVotes > 0) {
+                distributionCandidates.push(candidatePublicKey);
+              }
+            }
+          } else {
+            distributionCandidates.push(candidatePublicKey);
+          }
+        }
+      }
+
+      // now distribute the rewards based on the valid submissions
+      // Here it is assumed that all the nodes doing valid submission gets the same reward
+
+      const reward =
+        taskAccountDataJSON.bounty_amount_per_round /
+        distributionCandidates.length;
+      console.log('REWARD RECEIVED BY EACH NODE', reward);
+      for (let i = 0; i < distributionCandidates.length; i++) {
+        distributionList[distributionCandidates[i]] = reward;
+      }
+      console.log('Distribution List', distributionList);
+      return distributionList;
+    } catch (err) {
+      console.log('ERROR IN GENERATING DISTRIBUTION LIST', err);
+    }
+  }
+
+  async submitDistributionList(round) {
+    // This function just upload your generated dustribution List and do the transaction for that
+
+    console.log('SubmitDistributionList called');
+
+    try {
+      const distributionList = await this.generateDistributionList(round);
+
+      const decider = await namespaceWrapper.uploadDistributionList(
+        distributionList,
+        round,
+      );
+      console.log('DECIDER', decider);
+
+      if (decider) {
+        const response =
+          await namespaceWrapper.distributionListSubmissionOnChain(round);
+        console.log('RESPONSE FROM DISTRIBUTION LIST', response);
+      }
+    } catch (err) {
+      console.log('ERROR IN SUBMIT DISTRIBUTION', err);
+    }
+  }
+
+  async validateNode(submission_value, round) {
+    // Write your logic for the validation of submission value here and return a boolean value in response
+    // The sample logic can be something like mentioned below to validate the submission
+    try {
+      console.log('Received submission_value', submission_value, round);
+      const generatedValue = await namespaceWrapper.storeGet('cid');
+      console.log('GENERATED VALUE', generatedValue);
+      if (generatedValue == submission_value) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (err) {
+      console.log('ERROR  IN VALDIATION', err);
+      return false;
+    }
+  }
+
+  async shallowEqual(object1, object2) {
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2);
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
+    for (let key of keys1) {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  validateDistribution = async (
+    distributionListSubmitter,
+    round,
+    _dummyDistributionList,
+    _dummyTaskState,
+  ) => {
+    // Write your logic for the validation of submission value here and return a boolean value in response
+    // this logic can be same as generation of distribution list function and based on the comparision will final object , decision can be made
+
+    try {
+      console.log('Distribution list Submitter', distributionListSubmitter);
+      const rawDistributionList = await namespaceWrapper.getDistributionList(
+        distributionListSubmitter,
+        round,
+      );
+      let fetchedDistributionList;
+      if (rawDistributionList == null) {
+        fetchedDistributionList = _dummyDistributionList;
+      } else {
+        fetchedDistributionList = JSON.parse(rawDistributionList);
+      }
+      console.log('FETCHED DISTRIBUTION LIST', fetchedDistributionList);
+      const generateDistributionList = await this.generateDistributionList(
+        round,
+        _dummyTaskState,
+      );
+
+      // compare distribution list
+
+      const parsed = fetchedDistributionList;
+      console.log(
+        'compare distribution list',
+        parsed,
+        generateDistributionList,
+      );
+      const result = await this.shallowEqual(parsed, generateDistributionList);
+      console.log('RESULT', result);
+      return result;
+    } catch (err) {
+      console.log('ERROR IN VALIDATING DISTRIBUTION', err);
+      return false;
+    }
+  };
+  // Submit Address with distributioon list to K2
+  async submitTask(roundNumber) {
+    console.log('submitTask called with round', roundNumber);
+    try {
+      console.log('inside try');
+      console.log(
+        await namespaceWrapper.getSlot(),
+        'current slot while calling submit',
+      );
+      const submission = await this.fetchSubmission();
+      console.log('SUBMISSION', submission);
+      await namespaceWrapper.checkSubmissionAndUpdateRound(
+        submission,
+        roundNumber,
+      );
+      console.log('after the submission call');
+    } catch (error) {
+      console.log('error in submission', error);
+    }
+  }
+
+  async auditTask(roundNumber) {
+    console.log('auditTask called with round', roundNumber);
+    console.log(
+      await namespaceWrapper.getSlot(),
+      'current slot while calling auditTask',
+    );
+    await namespaceWrapper.validateAndVoteOnNodes(
+      this.validateNode,
+      roundNumber,
+    );
+  }
+
+  async auditDistribution(roundNumber) {
+    console.log('auditDistribution called with round', roundNumber);
+    await namespaceWrapper.validateAndVoteOnDistributionList(
+      this.validateDistribution,
+      roundNumber,
+    );
+  }
+}
+const coreLogic = new CoreLogic();
+
+module.exports = { coreLogic };
---
+++ text.md
@@ -0,0 +1,86 @@
+taskAccountInfo [
+task_node  |   {
+task_node  |     publickey: 'J7x6jfGEtE7CbivKudSkubfoa3XHNAf8heJsFYviLHjs',
+task_node  |     data: {
+task_node  |       task_name: 'get-k2-nodes',
+task_node  |       task_description: 'get k2 nodes',
+task_node  |       task_manager: [Array],
+task_node  |       is_whitelisted: false,
+task_node  |       is_active: true,
+task_node  |       task_audit_program: 'main',
+task_node  |       stake_pot_account: [Array],
+task_node  |       submissions: {},
+task_node  |       submissions_audit_trigger: {},
+task_node  |       total_bounty_amount: 10000000000,
+task_node  |       bounty_amount_per_round: 1000000000,
+task_node  |       total_stake_amount: 1000000000,
+task_node  |       minimum_stake_amount: 1000000000,
+task_node  |       available_balances: {},
+task_node  |       stake_list: [Object],
+task_node  |       ip_address_list: {},
+task_node  |       round_time: 1000,
+task_node  |       starting_slot: 13739810,
+task_node  |       audit_window: 500,
+task_node  |       submission_window: 300,
+task_node  |       task_executable_network: 'DEVELOPMENT',
+task_node  |       distribution_rewards_submission: {},
+task_node  |       distributions_audit_trigger: {},
+task_node  |       distributions_audit_record: {},
+task_node  |       task_metadata: '',
+task_node  |       task_vars: '',
+task_node  |       koii_vars: '',
+task_node  |       is_migrated: false,
+task_node  |       migrated_to: '',
+task_node  |       allowed_failed_distributions: 3
+task_node  |     }
+task_node  |   }
+task_node  | ]
+
+
+
+
+
+Cluster RPC URL: https://k2-testnet.koii.live
+task_node  | *********************ENDPOINTS*********************
+task_node  | /attention*
+task_node  | /
+task_node  | /nodes/:taskId
+task_node  | /register-node/:taskId
+task_node  | /get-all-running-tasks
+task_node  | /get-leveldb-stats
+task_node  | /namespace-wrapper
+task_node  | /tasks
+task_node  | *********************END ENDPOINTS*********************
+
+
+
+
+
+              Express server started @ http://localhost:8080
+task_node  | SETTING UP EXPRESS
+task_node  | setup function called
+task_node  | get-k2-nodes listening on port 10000
+task_node  | Console Ninja failed to send logs, restarting the process may help: failed to connect to host: failed to find and load WebSocket
+task_node  | Console Ninja failed to send logs, restarting the process may help: failed to connect to host: failed to find and load WebSocket
+task_node  | Console Ninja failed to send logs, restarting the process may help: failed to connect to host: failed to find and load WebSocket
+task_node  |
+task_node  |
+task_node  | Cluster RPC URL: https://k2-testnet.koii.live
+task_node  | https://k2-testnet.koii.live RPC URL
+task_node  | { ipAddresses: {} }
+task_node  | {}
+task_node  | { J7x6jfGEtE7CbivKudSkubfoa3XHNAf8heJsFYviLHjs: [] }
+task_node  | Running periodic jobs
+task_node  | Propagating Registry
+task_node  | nodesRegistry-J7x6jfGEtE7CbivKudSkubfoa3XHNAf8heJsFYviLHjs Not found
+task_node  | Task J7x6jfGEtE7CbivKudSkubfoa3XHNAf8heJsFYviLHjs has no valid nodes to hit
+task_node  | nodesRegistry-J7x6jfGEtE7CbivKudSkubfoa3XHNAf8heJsFYviLHjs Not found
+task_node  | {
+task_node  |   rss: 92676096,
+task_node  |   heapTotal: 34701312,
+task_node  |   heapUsed: 22982312,
+task_node  |   external: 3538645,
+task_node  |   arrayBuffers: 2601493
+task_node  | }
+task_node  | Submitter key 5miXLstzM9QrspG3WEGZRNWpiErBL1Pj9R4V2pttZoyt
+task_node  | Distribution key Aa7itkyXDXhbjuDgp2Xa35qbs6aFMgr9mVn79xEtogxq
